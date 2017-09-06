f33a401ee3f1db8c4706be50776f9f289ec36c06
carrotkr <31353801+carrotkr@users.noreply.github.com>
2017-09-05 18:08:39 -0700

    Create BinaryIterativeSearch.java

---
+++ BinaryIterativeSearch.java
@@ -0,0 +1,30 @@
+package edu.usfca.cs.cs245;
+
+public class BinaryIterativeSearch implements Practice2Search {
+
+	@Override
+	public String searchName() {
+		return "BinaryIterativeSearch";
+	}
+
+	@Override
+	public int search(int[] arr, int target) {	
+		int start = 0;
+		int end = arr.length - 1;
+		
+		while (start <= end) {
+			int mid = ((end - start) / 2) + start;
+			
+			// target is in arr[low...high] or not present.
+			if (target < arr[mid]) {
+				end = mid - 1;
+			} else if (target > arr[mid]) {
+				start = mid + 1;
+			} else {
+				return mid;
+			}
+		}
+		
+		return -1;
+	}
+}
